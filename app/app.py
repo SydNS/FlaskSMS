1793d6b0ceac1640a60667770955bba8e0d890b2
SydNS <sydneynsiimbe@gmail.com>
2023-01-23 10:08:47 +0300

    moves initialization to the init file

--- app/app.py
+++ app/app.py
@@ -1,11 +1,27 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
+from app import app
 @app.route('/')
 def hello_world():
     return 'Hey ya, Im starting here'
+
+
+@app.route('/')
+def index():
+    return 'Welcome to my Flask app!'
+
+
+@app.route('/hello')
+def hello():
+    name = app.request.args.get('name')
+    return f'Hello, {name}!'
+
+
+@app.route('/add', app.methods['POST'])
+def add():
+    data = app.request.get_json()
+    a = data['a']
+    b = data['b']
+    result = a + b
+    return app.jsonify({'result': result})
 
 
 if __name__ == '__main__':
